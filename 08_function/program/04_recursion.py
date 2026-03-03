abeccb700d0bd983abd33d53f69e384f62cbe3aa
Sourabh037 <bhandarisourabh2003@gmail.com>
2026-03-03 18:17:14 +0530

    edit

---
+++ 08_function/program/04_recursion.py
@@ -0,0 +1,12 @@
+''' factorial(5) = 5 X 4 X 3 X 2 X 1
+factorial(n) = n X n-1 X........3 X 2 X 1
+factorial(n) = n * factorial(n-1) '''
+                    # |___________this is the formula we used in recursion
+
+def factorial(n):
+    if(n==1 or n==0):
+        return 1
+    return n * factorial(n-1)
+
+n = int(input("enter a number : "))
+print(f"the factorial of this number is : {factorial(n)}")
